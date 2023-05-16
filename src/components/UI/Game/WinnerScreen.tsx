5ff3fd301469f8d88e76e312cdd843803b8858d5
ArmandoCode <totovip1464@gmail.com>
2023-05-16 11:52:09 -0600

    WinnerScreen.tsx: Rediseño los estilos de ciertos elementos

--- src/components/UI/Game/WinnerScreen.tsx
+++ src/components/UI/Game/WinnerScreen.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { type ILeaderboard } from '../../../interfaces/ILeaderboard'
 import { fireworks } from '../../../utils'
+import '../../../styles/loades.css'
 
 interface IProps {
   leaderboard: ILeaderboard[]
@@ -26,19 +27,66 @@ export const WinnerScreen = ({ leaderboard }: IProps) => {
   }
 
   return (
-        <section className="w-full flex items-center flex-col pt-20 pb-20">
-            {previewScreenWinner && (
-                <h1 className="text-7xl text-white animate-bounce">Y EL GANADOR ES...</h1>
-            )}
-            {!previewScreenWinner && (
-                <h2 className="text-7xl text-yellow-300 animate-pulse">{ `1° - ${topWinners[0].player}` }</h2>
-            )}
-            {showRestOfWinners && (
-                <div className='flex h-full flex-row items-center gap-7 mt-8'>
-                    <h2 className="text-4xl text-gray-400">{ topWinners.length > 1 ? `2° - ${topWinners[1].player}` : '' }</h2>
-                    <h2 className="text-3xl text-yellow-700 pt-2">{ topWinners.length > 2 ? `3° - ${topWinners[2].player}` : '' }</h2>
+        <>
+        {previewScreenWinner && (
+          <section className="w-full min-h-screen bg-gradient-to-b flex flex-col items-center justify-center gap-20">
+            <h2 className="text-[#E3E6E8] font-bold tracking-tight text-6xl">Cargando Podium</h2>
+            <div className="line-wobble"></div>
+          </section>
+        )}
+      {!previewScreenWinner && (
+        <section className="w-full h-screen flex items-center justify-between bg-gradient-to-b flex-col text-[#E3E6E8] relative">
+          <h1 className="text-7xl font-bold tracking-tight pt-32">Podium</h1>
+          <div className="flex items-end h-[65vh]">
+          {topWinners.length > 1 && (
+              <div className="flex flex-col justify-end items-center gap-8 w-[300px] h-5/6">
+                <h2 className="bg-[#FFFFFF26] rounded-full py-3 px-5 text-center text-3xl font-semibold min-w-[200px]">
+                  {topWinners[1].player}
+                </h2>
+                <div className="flex justify-center bg-[#DFDFDF] rounded-tl-2xl pt-14 flex-1 w-full">
+                  <div className="bg-[#F0F0F0] h-fit rounded-full py-5 px-10 text-8xl font-bold text-[#ABABAB]">
+                    2
+                  </div>
                 </div>
+              </div>
+          )}
+            <div className="flex flex-col justify-end items-center gap-8 w-[350px] h-full">
+              <h2 className="bg-[#FFFFFF26] rounded-full py-3 px-5 text-center text-3xl font-semibold min-w-[200px]">
+                {topWinners[0].player}
+              </h2>
+              <div className="flex justify-center bg-[#FFC727] rounded-t-2xl pt-14 flex-1 h-5/6 h-400px w-full shadow-2xl">
+                <div className="bg-[#FFD457] h-fit rounded-full py-5 px-10 text-8xl font-bold">
+                  1
+                </div>
+              </div>
+            </div>
+            {topWinners.length > 2 && (
+              <div className="flex flex-col justify-end items-center gap-8 w-[300px] h-4/6">
+                <h2 className="bg-[#FFFFFF26] rounded-full py-3 px-5 text-center text-3xl font-semibold min-w-[200px]">
+                  {topWinners[2].player}
+                </h2>
+                <div className="flex justify-center bg-[#FFA750] rounded-tr-2xl pt-14 h-full w-full shadow-inner">
+                  <div className="bg-[#FFC892] h-fit rounded-full py-5 px-10 text-8xl font-bold text-[#C66300]">
+                    3
+                  </div>
+                </div>
+              </div>
             )}
+          </div>
+          <div className="absolute bottom-2 right-5">
+            <small className='text-[#8d8d8d]'>
+              Design by{' '}
+              <a
+                href="https://github.com/JeremyDevCode"
+                target="_blank"
+                className="text-[#8d8d8d] font-medium" rel="noreferrer"
+              >
+                JeremyDev
+              </a>
+            </small>
+          </div>
         </section>
+      )}
+        </>
   )
 }

704757405d32f6c46e2e01d49e3df24110c996a2
ArmandoCode <totovip1464@gmail.com>
2023-05-16 11:45:47 -0600

    Home.tsx: Cambio el titulo de la pantalla principal

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,4 @@
-import kapool from '../assets/kapool-removebg.png'
-import { NavLink } from 'react-router-dom'
-import { Boton, Navbar } from './UI'
+import { Navbar, WelcomeTitle } from './UI'
 
 export const Home = () => {
   return (
@@ -8,18 +6,7 @@ export const Home = () => {
           <Navbar />
           <section className='flex justify-center w-6/12 '>
           <div className="flex items-center flex-col">
-            <img src={kapool} />
-            {/* <NavLink to='/create'>
-              <Boton type='button' message='Crear partida'/>
-            </NavLink>
-            <NavLink to='/join'>
-              <Boton type='button' message='Ingresar a partida con codigo'/>
-            </NavLink> */}
-            <NavLink to='/join'>
-              <button className='w-72 font-mono scale bg-blue-700 p-2 text-white font-bold rounded mt-8 ease-in duration-100 hover:scale-110'
-               >Ingresar a partida con código
-              </button>
-            </NavLink>
+            <WelcomeTitle />
           </div>
           </section>
         </>

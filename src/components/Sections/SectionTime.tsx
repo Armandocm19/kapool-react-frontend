e8a276dc78e13bd9d3ed73c59b1f4df1d9d2a937
ArmandoCode <totovip1464@gmail.com>
2023-05-16 11:50:12 -0600

    SectionTime.tsx: Rediseño los estilos de ciertos elementos

--- src/components/Sections/SectionTime.tsx
+++ src/components/Sections/SectionTime.tsx
@@ -8,16 +8,16 @@ interface Props {
 export const SectionTime = ({ onChangeTime, timeForQuestion }: Props) => {
   return (
     <div className="flex items-center gap-3 mt-5">
-      <label className='text-white'>Tiempo para responder:</label>
+      <label className='text-white font-bold tracking-tight'>Tiempo para responder:</label>
       <Input
         type="number"
         name='answer4'
         onChange={onChangeTime}
         value={ timeForQuestion }
-        className='outline-0 shadow w-20 bg-zinc-800 h-10 rounded placeholder:text-zinc-500 pl-4 text-zinc-500 focus:shadow-zinc-500 ease-in duration-100 pr-1'
+        className='outline-0 font-bold tracking-tight shadow w-20 bg-[#4F5D75]/30 h-10 rounded placeholder:text-zinc-500 pl-4 text-zinc-500 focus:shadow-zinc-500 ease-in duration-100 pr-1'
         placeholder='En segundos...'
       />
-      <label className='text-white'>60s (max)</label>
+      <label className='text-white font-bold tracking-tight'>60s (max)</label>
     </div>
   )
 }

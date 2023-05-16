ccf2b8bc8dc23b8be39f81c7decd1e0ffe64dc78
ArmandoCode <totovip1464@gmail.com>
2023-05-16 11:47:18 -0600

    JoinGame.tsx: Rediseño los estilos de la pagina

--- src/components/JoinGame.tsx
+++ src/components/JoinGame.tsx
@@ -90,17 +90,17 @@ export const JoinGame = () => {
           !showWaitingScreen
             ? (
             <div className="flex items-center flex-col">
-            <h1 className='text-white text-5xl'>{!dataFromServer.code ? 'Ingresa el código' : 'Ingresa tu nombre'}</h1>
+            <h1 className='text-white font-bold tracking-tight text-5xl'>{!dataFromServer.code ? 'Ingresa el código' : 'Ingresa tu nombre'}</h1>
             <Input
-                className='outline-0 w-64 shadow bg-zinc-800 h-10 rounded placeholder:text-zinc-500 pl-4 text-zinc-500 mt-8 focus:shadow-zinc-500 ease-in duration-100'
+                className='outline-0 w-64 font-bold tracking-tight shadow bg-zinc-800 h-10 rounded placeholder:text-zinc-500 pl-4 text-zinc-500 mt-8 focus:shadow-zinc-500 ease-in duration-100'
                 type='text'
                 onChange={onChangeData}
                 value={!dataFromServer.code ? codeValue.code : codeValue.name}
                 name={!dataFromServer.code ? 'code' : 'name'}
-                placeholder={!codeValue.code ? 'A9J3B3...' : 'John...'}
+                placeholder={!dataFromServer.code ? 'A9J3B3...' : 'John...'}
             />
             <Boton
-                className='w-72 font-mono scale bg-blue-700 p-2 text-white font-bold rounded mt-4 ease-in duration-100 hover:scale-110'
+                className='w-72 scale bg-[#EF8354] p-2 text-[#242634] font-bold rounded mt-4 ease-in duration-100 hover:scale-110'
                 message='Ingresar'
                 type='submit'
             />
@@ -108,8 +108,8 @@ export const JoinGame = () => {
             {
                 error.errorValue && (
                     <div className="bg-red-600 p-3 rounded mt-5">
-                        <h2 className='text-center text-white text-xl font-bold'>¡Oh no! :(</h2>
-                        <h3 className='text-center text-white'>{ error.message }</h3>
+                        <h2 className='text-center text-white text-xl font-bold tracking-tight'>¡Oh no! :(</h2>
+                        <h3 className='text-center text-white font-bold tracking-tight'>{ error.message }</h3>
                     </div>
                 )
             }
@@ -117,7 +117,7 @@ export const JoinGame = () => {
               )
             : (
             <div className="flex items-center flex-col">
-              <h1 className='text-white text-5xl'>Esperando a que el juego inicie...</h1>
+              <h1 className='text-white font-bold tracking-tight text-5xl'>Esperando a que el juego inicie...</h1>
             </div>
               )
         }

0206971cbe0eeade039b61d4448e7103cc6a9d73
ArmandoCode <totovip1464@gmail.com>
2023-05-16 15:50:53 -0600

    Mejor el diseño del área de autenticacion

--- src/components/AuthRegister.tsx
+++ src/components/AuthRegister.tsx
@@ -22,38 +22,41 @@ export const AuthRegister = () => {
             {
                 ({ handleReset, values, errors, setErrors }) => (
                     <Form noValidate className='flex flex-col justify-center w-6/12 items-center'>
-                        <h1 className='text-5xl font-mono text-white'>Create una cuenta</h1>
+                        <h1 className='text-5xl font-bold tracking-tight text-white'>Create una cuenta</h1>
                         <InputForm
                             type='text'
                             name='username'
-                            className='outline-0 shadow w-6/12 bg-zinc-800 h-10 rounded placeholder:text-zinc-500 pl-4 text-zinc-500 mt-8 focus:shadow-zinc-500 ease-in duration-100'
+                            className='outline-0 font-bold tracking-tight shadow w-6/12 bg-[#4F5D75]/30 h-10 rounded placeholder:text-zinc-500 pl-4
+                            text-zinc-500 mt-8 focus:shadow-zinc-500 ease-in duration-100'
                             placeholder='Nombre de usuario'
                         />
                         <InputForm
                             type='email'
                             name='email'
-                            className='outline-0 shadow w-6/12 bg-zinc-800 h-10 rounded placeholder:text-zinc-500 pl-4 text-zinc-500 mt-5 focus:shadow-zinc-500 ease-in duration-100'
+                            className='outline-0 font-bold tracking-tight shadow w-6/12 bg-[#4F5D75]/30 h-10 rounded placeholder:text-zinc-500 pl-4
+                            text-zinc-500 mt-8 focus:shadow-zinc-500 ease-in duration-100'
                             placeholder='Correo electrónico'
                         />
                         <InputForm
-                            type='text'
+                            type='password'
                             name='password'
-                            className='outline-0 shadow w-6/12 bg-zinc-800 h-10 rounded placeholder:text-zinc-500 pl-4 text-zinc-500 mt-5 focus:shadow-zinc-500 ease-in duration-100'
+                            className='outline-0 font-bold tracking-tight shadow w-6/12 bg-[#4F5D75]/30 h-10 rounded placeholder:text-zinc-500 pl-4
+                            text-zinc-500 mt-8 focus:shadow-zinc-500 ease-in duration-100'
                             placeholder='Contraseña'
                         />
 
                         <Boton
                             type='submit'
                             message='Registrar cuenta'
-                            className='w-72 font-mono scale bg-blue-700 p-2 text-white font-bold rounded mt-4 ease-in duration-100 hover:scale-110'
+                            className='w-72 font-bold tracking-tight scale bg-[#EF8354] p-2 text-[#242634] rounded mt-4 ease-in duration-100 hover:scale-110'
                         />
                     </Form>
                 )
             }
             </Formik>
             <div className="flex justify-center items-center mt-5 flex-col">
-                <p className="text-white">¿Ya tienes cuenta?</p>
-                <NavLink to="/login" className="text-blue-700">Inicia sección</NavLink>
+                <p className="text-white font-bold tracking-tight">¿Ya tienes cuenta?</p>
+                <NavLink to="/login" className="text-blue-700 font-bold tracking-tight underline">Inicia sección</NavLink>
             </div>
         </div>
   )

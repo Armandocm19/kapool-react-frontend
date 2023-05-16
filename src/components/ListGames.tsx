4388fdd1586104f6036d2097c2b055ee57c17ffd
ArmandoCode <totovip1464@gmail.com>
2023-05-16 11:47:42 -0600

    ListGames.tsx: Rediseño el estilo de las tablas

--- src/components/ListGames.tsx
+++ src/components/ListGames.tsx
@@ -36,21 +36,21 @@ export const ListGames = () => {
             gamesByOwner.length !== 0
               ? (
                     <>
-                      <h1 className="text-5xl font-mono text-white">Tus partidas creadas</h1>
-                      <table className="w-8/12 text-center text-sm font-light mt-10">
-                          <thead className="border-b font-medium dark:border-neutral-500">
-                            <tr className="border-b font-medium dark:border-neutral-500">
-                                <th scope="col" className="px-6 py-4 text-white">Número</th>
-                                <th scope="col" className="px-6 py-4 text-white">Código</th>
-                                <th scope="col" className="px-6 py-4 text-white">Cantidad de jugadores</th>
-                                <th scope="col" className="px-6 py-4 text-white">Estado</th>
-                                <th scope="col" className="px-6 py-4 text-white">Acciones</th>
+                      <h1 className="text-5xl font-bold text-white">Tus partidas creadas</h1>
+                      <table className="w-8/12 text-center text-sm font-light mt-10 rounded-lg overflow-hidden">
+                          <thead className="border-b font-medium bg-[#4F5D75]/30">
+                            <tr className="border-b font-medium border-[#20232F]">
+                                <th scope="col" className="px-6 py-4 text-white font-bold">Número</th>
+                                <th scope="col" className="px-6 py-4 text-white font-bold">Código</th>
+                                <th scope="col" className="px-6 py-4 text-white font-bold">Cantidad de jugadores</th>
+                                <th scope="col" className="px-6 py-4 text-white font-bold">Estado</th>
+                                <th scope="col" className="px-6 py-4 text-white font-bold">Acciones</th>
                             </tr>
                           </thead>
                         <tbody className="overflow-y-scroll">
                         {
                           gamesByOwner.map((game, index) => (
-                              <tr key={game.hostId} className="border-b border-neutral-700 bg-neutral-800 text-neutral-50 dark:border-neutral-600 dark:bg-neutral-700">
+                              <tr key={game.hostId} className="border-b border-[#20232F] text-neutral-50 bg-[#4F5D75]/30">
                                   <td className="whitespace-nowrap px-6 py-2 font-medium">{ index + 1 }</td>
                                   <td className="whitespace-nowrap px-6 py-2">{ game.hostId }</td>
                                   <td className="whitespace-nowrap px-6 py-2">{ `${game.playerList?.length}/10` }</td>
@@ -65,7 +65,7 @@ export const ListGames = () => {
                                          <button className="w-full bg-blue-700 text-white font-bold rounded p-2" onClick={async () => { await activeGame(game) }}>Activar</button>
                                           )
                                   } */}
-                                  <button className="w-full bg-blue-700 text-white font-bold rounded p-2" onClick={async () => { await activeGame(game) }}>Activar</button>
+                                  <button className="w-full bg-[#EF8354] text-[#242634] font-bold rounded p-2 ease-in duration-100 hover:scale-105" onClick={async () => { await activeGame(game) }}>Activar</button>
                                   </td>
                               </tr>
                           ))

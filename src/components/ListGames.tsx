dee61a33b0e4b68e921736b91fa0f1762209fda1
ArmandoCode <totovip1464@gmail.com>
2023-09-20 19:07:52 -0600

    add responsive styles to list games

--- src/components/ListGames.tsx
+++ src/components/ListGames.tsx
@@ -18,7 +18,7 @@ export const ListGames = () => {
   }
 
   return (
-    <div className="w-screen flex justify-center flex-col items-center pt-20 pb-20">
+    <div className="w-[90%] overflow-hidden flex justify-center flex-col items-center pt-20 pb-20 lg:w-screen">
       <BackTo text="Volver al menú principal" to="/" />
       {isLoading
         ? (
@@ -27,80 +27,82 @@ export const ListGames = () => {
         : gamesByOwner.length !== 0
           ? (
         <>
-          <h1 className="text-5xl font-bold text-white">
+          <h1 className="text-2xl font-bold text-white md:text-3xl lg:text-5xl">
             Tus partidas creadas
           </h1>
-          <table className="w-8/12 text-center text-sm font-light mt-10 rounded-lg overflow-hidden">
-            <thead className="border-b font-medium bg-[#4F5D75]/30">
-              <tr className="border-b font-medium border-[#20232F]">
-                <th scope="col" className="px-6 py-4 text-white font-bold">
-                  Número
-                </th>
-                <th scope="col" className="px-6 py-4 text-white font-bold">
-                  Código
-                </th>
-                <th scope="col" className="px-6 py-4 text-white font-bold">
-                  Cantidad de jugadores
-                </th>
-                <th scope="col" className="px-6 py-4 text-white font-bold">
-                  Estado
-                </th>
-                <th scope="col" className="px-6 py-4 text-white font-bold">
-                  Acciones
-                </th>
-              </tr>
-            </thead>
-            <tbody className="overflow-y-scroll">
-              {gamesByOwner.map((game, index) => (
-                <tr
-                  key={game.hostId}
-                  className="border-b border-[#20232F] text-neutral-50 bg-[#4F5D75]/30"
-                >
-                  <td className="whitespace-nowrap px-6 py-2 font-medium">
-                    {index + 1}
-                  </td>
-                  <td className="whitespace-nowrap px-6 py-2">{game.hostId}</td>
-                  <td className="whitespace-nowrap px-6 py-2">{`${game.playerList?.length}/10`}</td>
-                  <td className="whitespace-nowrap px-6 py-2">
-                    {game.isLive ? 'Activado' : 'Desactivado'}
-                  </td>
-                  <td className="whitespace-nowrap  py-2 flex justify-center items-center">
-                    {game.isLive
-                      ? (
-                      <div className='w-full flex justify-center items-center gap-2'>
-                        <button
-                          className="w-auto bg-[#EF8354] text-[#242634] font-bold rounded p-2 ease-in duration-100 hover:scale-105"
-                          onClick={async () => {
-                            await restartGame(game)
-                          }}
-                        >
-                          Reiniciar
-                        </button>
-                        <button onClick={() => handleToRemoveGameInDB(game.quizId, game._id!)}>
-                          <RemoveIcon width='25' height='25' className='text-red-600' />
-                        </button>
-                      </div>
-                        )
-                      : (
-                      <div className='w-full flex justify-center items-center gap-2'>
-                        <button
-                          className="w-auto bg-[#EF8354] text-[#242634] font-bold rounded px-3 py-2 ease-in duration-100 hover:scale-105"
-                          onClick={async () => {
-                            await activeGame(game)
-                          }}
-                        >
-                          Activar
-                        </button>
-                        <button onClick={() => handleToRemoveGameInDB(game.quizId, game._id!)}>
-                          <RemoveIcon width='25' height='25' className='text-red-600 cursor-pointer' />
-                        </button>
-                      </div>
-                        )}
-                  </td>
+          <div className="w-full h-auto max-h-72 overflow-x-auto overflow-y-auto mt-5 md:w-8/12 md:h-96 md:max-h-[500px] scroll">
+            <table className="w-full text-center text-sm font-light mt-10 rounded-lg">
+              <thead className="border-b font-medium bg-[#4F5D75]/30">
+                <tr className="border-b font-medium border-[#20232F]">
+                  <th scope="col" className="px-6 py-4 text-white font-bold">
+                    Número
+                  </th>
+                  <th scope="col" className="px-6 py-4 text-white font-bold">
+                    Código
+                  </th>
+                  <th scope="col" className="px-6 py-4 text-white font-bold">
+                    Cantidad de jugadores
+                  </th>
+                  <th scope="col" className="px-6 py-4 text-white font-bold">
+                    Estado
+                  </th>
+                  <th scope="col" className="px-6 py-4 text-white font-bold">
+                    Acciones
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody className="overflow-y-scroll">
+                {gamesByOwner.map((game, index) => (
+                  <tr
+                    key={game.hostId}
+                    className="border-b border-[#20232F] text-neutral-50 bg-[#4F5D75]/30"
+                  >
+                    <td className="whitespace-nowrap px-6 py-2 font-medium">
+                      {index + 1}
+                    </td>
+                    <td className="whitespace-nowrap px-6 py-2">{game.hostId}</td>
+                    <td className="whitespace-nowrap px-6 py-2">{`${game.playerList?.length}/10`}</td>
+                    <td className="whitespace-nowrap px-6 py-2">
+                      {game.isLive ? 'Activado' : 'Desactivado'}
+                    </td>
+                    <td className="whitespace-nowrap  py-2 flex justify-center items-center">
+                      {game.isLive
+                        ? (
+                        <div className='w-full flex justify-center items-center gap-2'>
+                          <button
+                            className="w-auto bg-[#EF8354] text-[#242634] font-bold rounded p-2 ease-in duration-100 hover:scale-105"
+                            onClick={async () => {
+                              await restartGame(game)
+                            }}
+                          >
+                            Reiniciar
+                          </button>
+                          <button onClick={() => handleToRemoveGameInDB(game.quizId, game._id!)}>
+                            <RemoveIcon width='25' height='25' className='text-red-600' />
+                          </button>
+                        </div>
+                          )
+                        : (
+                        <div className='w-full flex justify-center items-center gap-2'>
+                          <button
+                            className="w-auto bg-[#EF8354] text-[#242634] font-bold rounded px-3 py-2 ease-in duration-100 hover:scale-105"
+                            onClick={async () => {
+                              await activeGame(game)
+                            }}
+                          >
+                            Activar
+                          </button>
+                          <button onClick={() => handleToRemoveGameInDB(game.quizId, game._id!)}>
+                            <RemoveIcon width='25' height='25' className='text-red-600 cursor-pointer' />
+                          </button>
+                        </div>
+                          )}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </>
             )
           : (
